c021f21d9bb06ea500727a312280fde797f3912b
choMatt <mattseneris18@gmail.com>
2023-06-28 14:29:37 +0800

    added Sign In and Sign Up components

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignIn = function(){
+    return (
+        <></>
+    )
+}
+
+export default SignIn
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const SignUp = function() {
+    return (
+        <></>
+    )
+}
+
+export default SignUp
