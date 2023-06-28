b858c8812dd9408e0d8b8c198380271bae738e96
choMatt <mattseneris18@gmail.com>
2023-06-28 17:09:59 +0800

    added ed authentication logic on Sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
 import { useState } from 'react'
 import './root.css'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
 function App() {
+  const [isSignIn, setIsSignIn] = useState(false)
+
+  function clickHandler(){
+    setIsSignIn(!isSignIn)
+    setFormData({ email: '', password: '' });
+  }
+
+  const [accounts, setAccounts] = useState([])
+  console.log(accounts)
+
+  const [formData, setFormData] = useState({
+    email:'',
+    password:''
+  })
+    
+  function handleChange(event) {
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [event.target.name]: event.target.value
+        }
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (formData.email && formData.password) {
+      setAccounts(prevAccounts => [...prevAccounts, formData]);
+    } else {
+      console.error('input required')
+    }
+  }
+
+
+  const authComponent = isSignIn ? (
+      <SignIn clickFunction={clickHandler} />
+    ) : (
+      <SignUp
+        clickFunction={clickHandler}
+        inputHandle={handleChange}
+        submit={handleSubmit}
+      />
+  );
 
   return (
-    <>
-    </>
+    <main>
+      {authComponent}
+    </main>
   )
 }
 
