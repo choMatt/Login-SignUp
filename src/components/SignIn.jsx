e8fe52bfbb7c89ceb25658888b0346e69ed0e1f8
choMatt <mattseneris18@gmail.com>
2023-06-28 17:11:38 +0800

    added Sign In form markup

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const SignIn = function(){
+const SignIn = function(props){
     return (
-        <></>
+        <div>
+            <h1>Sign In</h1>
+            <form action="">
+                <input type="text" placeholder='Email' /> 
+                <input type="text" placeholder='Password' /> 
+                <button>Sign In</button>
+            </form>
+            <a onClick={props.clickFunction} href='#'>Create ccount</a>
+        </div>
     )
 }
 
