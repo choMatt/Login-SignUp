93d827c3c7d625ed7ff226fc499c352cddc1abca
choMatt <mattseneris18@gmail.com>
2023-06-28 17:10:56 +0800

    added sign up form markup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 
-const SignUp = function() {
+const SignUp = function(props) {
     return (
-        <></>
+        <div>
+            <h1>Sign Up</h1>
+            <form 
+                action=""
+                noValidate
+            >
+                <input 
+                    onChange={(e) => props.inputHandle(e)}
+                    type="text" 
+                    placeholder='Email' 
+                    name='email'
+                    required
+                /> 
+                <input 
+                    onChange={(e) => props.inputHandle(e)}
+                    type="password" 
+                    placeholder='Password' 
+                    name='password'
+                    required
+                /> 
+                <button onClick={props.submit}>Sign Up</button>
+            </form>
+            <a onClick={props.clickFunction} href='#'>Already have an account? Sign In here</a>
+        </div>
     )
 }
 
