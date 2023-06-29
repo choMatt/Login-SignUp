3c0e29dbd9084b4ea84ee9c7b74d47838ffd1b72
choMatt <mattseneris18@gmail.com>
2023-06-29 15:05:21 +0800

    renamed props.inputHandle to inputUpdate && added value to props.formData && also renamed the a tag onClick props to toggleAuthView && handleSubmit to submitForm

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 
 const SignUp = function(props) {
+    console.log(props)
     return (
         <div>
             <h1>Sign Up</h1>
@@ -10,22 +11,24 @@ const SignUp = function(props) {
                 noValidate
             >
                 <input 
-                    onChange={(e) => props.inputHandle(e)}
+                    onChange={(e) => props.inputUpdate(e)}
                     type="text" 
                     placeholder='Email' 
                     name='email'
+                    value= {props.formData.email}
                     required
                 /> 
                 <input 
-                    onChange={(e) => props.inputHandle(e)}
+                    onChange={(e) => props.inputUpdate(e)}
                     type="password" 
                     placeholder='Password' 
                     name='password'
+                    value= {props.formData.password}
                     required
                 /> 
-                <button onClick={props.submit}>Sign Up</button>
+                <button onClick={props.submitForm}>Sign Up</button>
             </form>
-            <a onClick={props.clickFunction} href='#'>Already have an account? Sign In here</a>
+            <a onClick={props.toggleAuthView} href='#'>Already have an account? Sign In here</a>
         </div>
     )
 }
